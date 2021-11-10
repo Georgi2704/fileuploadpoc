df28a6a35e390e974c6b7d4fa6eca4dbcb228092
Georgi2704 <jrmanev@gmail.com>
2021-11-10 11:48:54 +0100

    uncommented prod

--- server/api/api_v1/endpoints/fileupload.py
+++ server/api/api_v1/endpoints/fileupload.py
@@ -1,35 +1,45 @@
-# Copyright 2019-2020 SURF.
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from http import HTTPStatus
-from typing import List
-from uuid import UUID
-
-from fastapi import HTTPException
-from fastapi.param_functions import Body, Depends
 from fastapi.routing import APIRouter
 from starlette.responses import Response
+from fastapi import FastAPI, File, UploadFile
+import shutil
+import structlog
 
-from server.api import deps
-from server.api.deps import common_parameters
-from server.api.error_handling import raise_status
-from server.crud import map_crud
-from server.db.models import MapsTable, UsersTable
-from server.schemas import Map, MapCreate, MapCreateAdmin, MapUpdate, MapUpdateAdmin
+logger = structlog.get_logger(__name__)
 
 router = APIRouter()
+
+
+def name_file(column_name, record_name, image_name=""):
+    _, _, image_number = column_name.rpartition("_")[0:3]
+    current_name = image_name
+    extension = "png"  # todo: make it dynamic e.g. get it from mime-type, extra arg for this function?
+    if not current_name:
+        name = "".join([c if c.isalnum() else "-" for c in record_name])
+        name = f"{name}-{image_number}-1".lower()
+    else:
+        name, _ = current_name.split(".")
+        name, _, counter = name.rpartition("-")[0:3]
+        name = f"{name}-{int(counter) + 1}".lower()
+    name = f"{name}.{extension}"
+    logger.info("Named file", col_name=column_name, name_in=image_name, name_out=name)
+    return name
 
 
 @router.get("/")
 def get_hello(response: Response) -> str:
     return "Hello Formatics!"
+
+
+# stores in memory
+@router.post("/files/")
+async def create_file(file: bytes = File(...)):
+    return {"file_size": len(file)}
+
+# better for images
+@router.post("/uploadfile/")
+async def create_upload_file(image: UploadFile = File(...)):
+
+    with open("destination.png", "wb") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    return {"filename": image.filename}
--- server/main.py
+++ server/main.py
@@ -31,6 +31,13 @@ from server.exception_handlers.generic_exception_handlers import form_error_hand
 from server.forms import FormException
 from server.settings import app_settings
 from server.version import GIT_COMMIT_HASH
+from fastapi import WebSocket
+from fastapi.responses import HTMLResponse
+from typing import List
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+
 
 structlog.configure(
     processors=[
@@ -58,7 +65,7 @@ app = FastAPI(
     redoc_url="/api/redoc",
     version=GIT_COMMIT_HASH if GIT_COMMIT_HASH else "0.1.0",
     default_response_class=JSONResponse,
-    # root_path="/prod",
+    root_path="/prod",
     servers=[
         {
             "url": "https://postgres-boilerplate.renedohmen.nl",
@@ -85,10 +92,84 @@ app.add_middleware(
 app.add_exception_handler(FormException, form_error_handler)
 app.add_exception_handler(ProblemDetailException, problem_detail_handler)
 
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8080/ws/${client_id}`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 
-@app.router.get("/", response_model=str, response_class=JSONResponse, include_in_schema=False)
-def index() -> str:
-    return "FastAPI boilerplate backend root"
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
+
 
 
 logger.info("App is running")
